4e9b80d447fd480679780d30ebc8fb6f04d353af
ruben-arana <ruben.arana@infojobs.net>
2020-03-16 21:40:39 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # basketCoach
 Basketball webapp 4 coaches
+
+
+## Startup
+cd /webapp
+npm start
+go to http://localhost:3000
+
+
+./gradlew run
---
+++ src/test/kotlin/com/rubenaranamorera/BasketCoach/BasketCoachApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rubenaranamorera.BasketCoach
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasketCoachApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
